c1b1d9c1458e6a237ea931f5c065639832fceaef
王华君 <smart.dj@163.com>
2020-09-09 13:15:57 +0800

    添加es和ik

---
+++ config/启动常用集成开发环境.sh
@@ -0,0 +1,5 @@
+# docker-compose -f docker-compose-manage.yml up -d portainer
+docker-compose up -d nacos
+docker-compose up -d mysql
+docker-compose up -d redis
+docker-compose up -d yapi
---
+++ 启动常用集成开发环境.sh
@@ -0,0 +1,3 @@
+# docker-compose -f docker-compose-manage.yml up -d portainer
+docker-compose up -d nacos
+docker-compose up -d redis
